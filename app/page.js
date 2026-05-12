7a152d172ca4e7f9c9b2d6845b623f744bfd5bfd
malayakeys32-rgb <malayakeys32@gmail.com>
2026-05-12 13:23:44 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,2 @@
-import DashboardLayout from "../components/layout/DashboardLayout";
-import Header from "../components/ui/Header";
-import Sidebar from "../components/ui/Sidebar";
-import Container from "../components/ui/Container";
-import ChatBox from "../components/chat/ChatBox";
-
-export default function Home() {
-  return (
-    <DashboardLayout
-      sidebar={<Sidebar />}
-      header={<Header />}
-    >
-      <Container>
-        <ChatBox />
-      </Container>
-    </DashboardLayout>
-  );
-}
+import DashboardLayout from "@/components/layout/DashboardLayout";
+import ChatBox from "@/components/chat/ChatBox";
