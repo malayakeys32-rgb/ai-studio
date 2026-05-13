322c55621e9209d7a614233c6d03852023a8c279
malayakeys32-rgb <malayakeys32@gmail.com>
2026-05-13 02:48:31 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import ChatBox from "@/components/chat/ChatBox";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to AI Studio</h1>
+    <div style={{ padding: "20px" }}>
+      <ChatBox />
     </div>
   );
 }
