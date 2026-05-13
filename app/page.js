4657b30adec50c37a5396a19b186f12727d8501b
malayakeys32-rgb <malayakeys32@gmail.com>
2026-05-12 18:55:40 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,2 +1,11 @@
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import ChatBox from "@/components/chat/ChatBox";
+
+export default function Home() {
+  return (
+    <DashboardLayout>
+      <ChatBox />
+    </DashboardLayout>
+  );
+}
+
